2b7b561aa6eeeb9f9e17cf60277d8a8523e67983
yunjeongLim <630yjl@gmail.com>
2024-12-20 17:28:03 +0900

    Test: 테스트코드

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // app 디렉토리 활성화
+    },
+  };
+  
+  export default nextConfig;
+  
