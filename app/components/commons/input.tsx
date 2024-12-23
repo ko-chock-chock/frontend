16b44233e67c790992fc370a9abf38e641b82d56
630yjl <630yjl@gmail.com>
2024-12-23 14:52:04 +0000

    공통컴포넌트폴더와 인풋컴포넌트 생성

---
+++ app/components/commons/input.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface InputProps {
+  type?: "text" | "password" | "email" | "number"; // 필요한 타입 정의
+  placeholder?: string;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
+  error?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+  className,
+  error,
+}) => {
+  return (
+    <div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`${className} flex px-4 py-4 items-center gap-2 self-stretch rounded-lg border focus:border-[rgba(27,141,90,0.93)] focus:outline-none`}
+      />
+      {error && <span className="text-red-500 text-sm">{error}</span>}{" "}
+      {/* 오류 메시지 */}
+    </div>
+  );
+};
+
+export default Input;
