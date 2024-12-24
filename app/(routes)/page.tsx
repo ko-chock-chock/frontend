17f3db583fd1c9a66e8d3995013b0d04a9b8f8c2
630yjl <630yjl@gmail.com>
2024-12-24 14:07:40 +0000

    메인페이지 퍼블리싱 작업중, tailwind.config 추가

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,10 @@
+// 메인페이지
+import MainPage from "./_components/MainPage";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     test페이지수정333
-    </div>
+    <>
+      <MainPage />
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        main_bg_color: "#FEFEFE", // 메인 백그라운드 색상
+        main_text_color: "#35351E", // 메인 텍스트 컬러
+      },
+      fontFamily: {
+        sandoll: ['"Sandoll Tviceket"', "sans-serif"], // 폰트 디자인 => font-sandoll이라고 넣으면 됨
+        suit: ['"SUIT"', "sans-serif"],
       },
     },
   },
